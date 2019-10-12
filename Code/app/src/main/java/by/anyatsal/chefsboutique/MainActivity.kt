30554197e32a6ad87af09491ec472f6a88352b97
Anna Tsal <anna.tsal63@gmail.com>
2019-10-12 12:51:15 +0300

    added resources (layouts, icons, values)

--- Code/app/src/main/java/by/anyatsal/chefsboutique/MainActivity.kt
+++ Code/app/src/main/java/by/anyatsal/chefsboutique/MainActivity.kt
@@ -2,11 +2,23 @@ package by.anyatsal.chefsboutique
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(bottom_app_bar)
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_appbar_menu, menu)
+        return true
     }
 }
